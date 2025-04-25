7f4ef2e6018ab6d71c2619eb9d175fb30281eb35
just4phil <andre.schmode@googlemail.com>
2025-04-25 14:58:15 +0200

    updated rinas MarkerLEDs

--- src/definitions.h
+++ src/definitions.h
@@ -7,8 +7,8 @@
 // USE_ESP32 //USE_TEENSY wird hier nicht ausgewählt, sondern ist in der ini hinterlegt!
 //
 //--- LED-DEVICE --- activate EXACTLY ONE of these options: -------
-#define ANDRESGIT		// YULC1 auf COM5 
-//#define RINASBASS		// YULC2 auf COM17
+//#define ANDRESGIT		// YULC1 auf COM5 
+#define RINASBASS		// YULC2 auf COM17
 //#define SCROLLMATRIX 	// YULC4 auf COM10 - activate this for the klapp-Matrix
 //#define GITBOARD 		// TEENSY auf COM8 (aber beim teensy nicht nötig)
 //
--- src/songs.cpp
+++ src/songs.cpp
@@ -399,8 +399,8 @@ void Physical() {
 		progStrobo(1630, 82, 75, getRandomColorValue(), getRandomColorValue(), getRandomColorValue());
 		#ifdef BASS
 			// markerLED 1 - 3 in Benutzung bei Rina
-			markerLED5 = ASaite_Fis; // für Rina: bereits hier einschalten -> Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
-			markerLED6 = ASaite_E; // für Rina: bereits hier einschalten -> Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
+			//markerLED5 = ASaite_Fis; // für Rina: bereits hier einschalten -> Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
+			markerLED5 = ASaite_E; // für Rina: bereits hier einschalten -> Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
 		#endif		 
 		break;
 
@@ -408,8 +408,8 @@ void Physical() {
 		progFastBlingBling(13060, 2, 84);
 		#ifdef BASS
 			// markerLED 1 - 3 in Benutzung bei Rina
-			markerLED5 = ASaite_Fis; // für Rina: Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
-			markerLED6 = ASaite_E; // für Rina: bereits hier einschalten -> Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
+			//markerLED5 = ASaite_Fis; // für Rina: Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
+			markerLED5 = ASaite_E; // für Rina: bereits hier einschalten -> Ton liegt auf D-Saite: -> nach dem 2. refrain // TODO: geht nach diesem teil nicht mehr aus! auschalten!!
 		#endif
 		break;
 
@@ -417,7 +417,6 @@ void Physical() {
 		progFastBlingBling(11430, 6, 86);
 		#ifdef BASS
 			markerLED5 = 0; // für Rina: wieder auschalten!!
-			markerLED6 = 0;
 		#endif
 		break;
 
