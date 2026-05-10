d7b1f34886bee380e518cdf9d6abc2989a0700a5
just4phil <andre.schmode@googlemail.com>
2026-05-10 18:20:33 +0200

    maniac: fixed LED abschaltung in instrumental

--- src/songs.cpp
+++ src/songs.cpp
@@ -3641,11 +3641,25 @@ void Maniac() {
 		break;
 
 	case 40://instrumental a  12229
+		#ifdef GIT
+			markerLED1 = ESaite_F;
+			markerLED2 = ESaite_G;
+			markerLED3 = 0; //ESaite_Gis;
+			markerLED4 = ASaite_Gis;
+			markerLED5 = ASaite_G;
+		#endif	
 		//progMatrixHorizontal(12229, 42, 70, CRGB::Green);
 		matrixMovieFX(12229, 42, 100, 5);
 		break;
 
 	case 42://instrumental b  11465
+		#ifdef GIT
+			markerLED1 = ESaite_F;
+			markerLED2 = ESaite_G;
+			markerLED3 = 0; //ESaite_Gis;
+			markerLED4 = ASaite_Gis;
+			markerLED5 = ASaite_G;
+		#endif	
 		progFullColors(11465, 44, 756);
 		break;
 
@@ -3791,11 +3805,27 @@ void Maniac_Tminus1() {
 		break;
 
 	case 40://instrumental a  12229
+		#ifdef GIT
+			markerLED1 = ESaite_E;
+			markerLED2 = ESaite_Fis;
+			markerLED3 = 0;//ESaite_G;
+			markerLED4 = ESaite_B;
+			markerLED5 = ASaite_G;
+			markerLED6 = ASaite_Fis;
+		#endif
 		//progMatrixHorizontal(12229, 42, 70, CRGB::Green);
 		matrixMovieFX(12229, 42, 100, 5);
 		break;
 
 	case 42://instrumental b  11465
+		#ifdef GIT
+			markerLED1 = ESaite_E;
+			markerLED2 = ESaite_Fis;
+			markerLED3 = 0;//ESaite_G;
+			markerLED4 = ESaite_B;
+			markerLED5 = ASaite_G;
+			markerLED6 = ASaite_Fis;
+		#endif	
 		progFullColors(11465, 44, 756);	//TODO: passt hier nicht so gut!!!
 		break;
 
